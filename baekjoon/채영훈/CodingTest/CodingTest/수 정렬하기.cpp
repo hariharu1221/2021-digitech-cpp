80125d107ce2565efcc94485b491be952a9b2e18
kkulbeol <chaeyh04@gmail.com>
2021-07-27 16:29:05 +0900

    반례 못찾겠다 히히...

---
+++ baekjoon/채영훈/CodingTest/CodingTest/수 정렬하기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> v(2001);
+
+	int val;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> val;
+		if (val < 0)
+			v[abs(val)] = val;
+		else
+			v[val + 1000] = val;
+	}
+
+	reverse(v.begin(), v.begin() + 1001);
+
+	auto remove = remove_if(v.begin(), v.end(), [](int n) {return n == 0; });
+	v.resize(remove - v.begin());
+
+	for (auto& iter : v)
+		cout << iter << endl;
+
+	return 0;
+}
