3b6dcd321b16cf28760f2e9a82e1176c75602ef5
kkulbeol <chaeyh04@gmail.com>
2021-07-27 16:42:53 +0900

    히히 풀었다 히히

--- baekjoon/채영훈/CodingTest/CodingTest/수 정렬하기.cpp
+++ baekjoon/채영훈/CodingTest/CodingTest/수 정렬하기.cpp
@@ -10,13 +10,13 @@ int main()
 	int N;
 	cin >> N;
 
-	vector<int> v(2001);
+	vector<int> v(2001, -3000);
 
 	int val;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> val;
-		if (val < 0)
+		if (val <= 0)
 			v[abs(val)] = val;
 		else
 			v[val + 1000] = val;
@@ -24,7 +24,7 @@ int main()
 
 	reverse(v.begin(), v.begin() + 1001);
 
-	auto remove = remove_if(v.begin(), v.end(), [](int n) {return n == 0; });
+	auto remove = remove_if(v.begin(), v.end(), [](int n) {return n == -3000; });
 	v.resize(remove - v.begin());
 
 	for (auto& iter : v)
