00e9eda37d9ecb08e362b798ee9f2d6be560c905
박유한 <dbgksdl00@gmail.com>
2021-07-27 09:34:21 +0900

    * 작동 확인 코드

--- data-structure/main.cpp
+++ data-structure/main.cpp
@@ -3,6 +3,8 @@
 #include "cTimer.h"
 #include "cMyVector.h"
 #include <vector>
+#include <list>
+#include "cMyList.h"
 
 using namespace std;
 
@@ -12,46 +14,63 @@ constexpr int insert_count = 10000000;
 int main()
 {
     cTimer<double> timer;
-    cMyVector<double> my_time_data(loop_count);
-    double my_total_data = 0;
+    //cMyVector<double> my_time_data(loop_count);
+    //double my_total_data = 0;
+    //
+    //for (int i = 0; i < loop_count; ++i)
+    //{
+    //    timer.Play();
+    //    cMyVector<int> cMyVec;
+    //    for (int j = 0; j < insert_count; ++j)
+    //        cMyVec.push_back(j);
+    //
+    //    my_time_data[i] = timer.Pause();
+    //    my_total_data += my_time_data[i];
+    //}
+    //
+    //sort(my_time_data.begin(), my_time_data.end());
+    //cout << "직접 만든 벡터의 " << insert_count << "개 추가에 걸리는 시간" << endl;
+    //cout << "평균 소요 시간 : " << my_total_data / loop_count << "초\t";
+    //cout << "최소 소요 시간 : " << my_time_data[0] << "초\t";
+    //cout << "최대 소요 시간 : " << my_time_data[loop_count - 1] << "초" << endl;
+    //
+    //vector<double> stl_time_data(loop_count);
+    //double stl_total_data = 0;
+    //
+    //for (int i = 0; i < loop_count; ++i)
+    //{
+    //    timer.Play();
+    //    vector<int> cStlVec;
+    //    for (int j = 0; j < insert_count; ++j)
+    //        cStlVec.push_back(j);
+    //
+    //    stl_time_data[i] = timer.Pause();
+    //    stl_total_data += stl_time_data[i];
+    //}
+    //
+    //sort(stl_time_data.begin(), stl_time_data.end());
+    //cout << "STL 벡터의 " << insert_count << "개 추가에 걸리는 시간" << endl;
+    //cout << "평균 소요 시간 : " << stl_total_data / loop_count << "초\t";
+    //cout << "최소 소요 시간 : " << stl_time_data[0] << "초\t";
+    //cout << "최대 소요 시간 : " << stl_time_data[loop_count - 1] << "초" << endl;
 
-    for (int i = 0; i < loop_count; ++i)
+    cMyList<int> my_list;
+    my_list.push_back(5);
+    my_list.push_back(3);
+    my_list.push_back(2);
+    my_list.push_back(1);
+    my_list.push_back(4);
+
+    for (auto& iter : my_list)
     {
-        timer.Play();
-        cMyVector<int> cMyVec;
-        for (int j = 0; j < insert_count; ++j)
-            cMyVec.push_back(j);
-
-        my_time_data[i] = timer.Pause();
-        my_total_data += my_time_data[i];
-    }
-    
-    sort(my_time_data.begin(), my_time_data.end());
-    cout << "직접 만든 벡터의 " << insert_count << "개 추가에 걸리는 시간" << endl;
-    cout << "평균 소요 시간 : " << my_total_data / loop_count << "초\t";
-    cout << "최소 소요 시간 : " << my_time_data[0] << "초\t";
-    cout << "최대 소요 시간 : " << my_time_data[loop_count - 1] << "초" << endl;
-
-    vector<double> stl_time_data(loop_count);
-    double stl_total_data = 0;
-    
-    for (int i = 0; i < loop_count; ++i)
-    {
-        timer.Play();
-        vector<int> cStlVec;
-        for (int j = 0; j < insert_count; ++j)
-            cStlVec.push_back(j);
-    
-        stl_time_data[i] = timer.Pause();
-        stl_total_data += stl_time_data[i];
+        cout << iter << "\t";
     }
 
-    sort(stl_time_data.begin(), stl_time_data.end());
-    cout << "STL 벡터의 " << insert_count << "개 추가에 걸리는 시간" << endl;
-    cout << "평균 소요 시간 : " << stl_total_data / loop_count << "초\t";
-    cout << "최소 소요 시간 : " << stl_time_data[0] << "초\t";
-    cout << "최대 소요 시간 : " << stl_time_data[loop_count - 1] << "초" << endl;
-
+    my_list.pop_back();
+    my_list.pop_back();
+    my_list.pop_back();
+    my_list.pop_back();
+    my_list.pop_back();
 
     return 0;
 }
